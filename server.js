d540e76f4cc48f0365015957e11759d775a560e1
anuhyanarra <30872018+anuhyanarra@users.noreply.github.com>
2017-08-14 12:47:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'new_1.html'));
-});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'second_one.html'));
